3701c1a3eaa8d8308c02098609b4632ee001de3f
Tomasz <tomasz.j.krzywda@gmail.com>
2014-08-11 15:06:25 +0200

    Added module for parsing html string. App displays manga list correctly.

--- app/controllers/win/manga_list.js
+++ app/controllers/win/manga_list.js
@@ -1,5 +1,7 @@
 var args = arguments[0] || {};
 var http = require('http');
+var parserModule = require('pl.tkrz.mangareader.parser');
+var string = require('alloy/string');
 
 function init(){
     $.activityIndicator.show();
@@ -7,6 +9,21 @@ function init(){
 }
 
 function loadData(data){
-    Ti.API.info(JSON.stringify(data));
+	var string = JSON.stringify(data);
+    var list = parserModule.parseHtml(data);
+    list = JSON.parse(list);
     $.activityIndicator.hide();
+    displayList(list);
 }
+
+function displayList(list){
+	Ti.API.info(JSON.stringify(list));
+	var manga = Ti.UI.createListSection({
+		items: list
+	});
+	$.mangaList.appendSection(manga);
+}
+
+function openTitle(e){
+	alert(JSON.stringify(e));
+};
